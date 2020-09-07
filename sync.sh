b5817288a11639b5e8f275e86c1da5bab2d1afad
Chris Mutel <cmutel@gmail.com>
2020-09-07 13:40:53 +0200

    Add favicons

--- sync.sh
+++ sync.sh
@@ -1,2 +1,5 @@
 #!/opt/local/bin/bash
 rsync -avl --omit-dir-times --no-perms 2020/ vultr:websites/2020.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.png vultr:websites/2020.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.ico vultr:websites/2020.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.webmanifest vultr:websites/2020.brightcon.link/
