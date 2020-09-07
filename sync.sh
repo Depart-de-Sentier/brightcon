53365b040602345f59728e5326906ebeb149ff6f
Chris Mutel <cmutel@gmail.com>
2020-09-07 13:25:54 +0200

    Fix path in sync script

--- sync.sh
+++ sync.sh
@@ -1,2 +1,2 @@
 #!/opt/local/bin/bash
-rsync -avl --omit-dir-times --no-perms 2020 vultr:websites/2020.brightcon.link
+rsync -avl --omit-dir-times --no-perms 2020/ vultr:websites/2020.brightcon.link/
