cb34401cea62d2ed6382a3a5c9bc254f4b651f98
Chris Mutel <cmutel@gmail.com>
2021-07-15 19:08:56 +0200

    Add draft 2021 page

--- sync.sh
+++ sync.sh
@@ -3,3 +3,8 @@ rsync -avl --omit-dir-times --no-perms 2020/ vultr:websites/2020.brightcon.link/
 rsync -avl --omit-dir-times --no-perms *.png vultr:websites/2020.brightcon.link/
 rsync -avl --omit-dir-times --no-perms *.ico vultr:websites/2020.brightcon.link/
 rsync -avl --omit-dir-times --no-perms *.webmanifest vultr:websites/2020.brightcon.link/
+
+rsync -avl --omit-dir-times --no-perms 2021/ vultr:websites/2021.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.png vultr:websites/2021.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.ico vultr:websites/2021.brightcon.link/
+rsync -avl --omit-dir-times --no-perms *.webmanifest vultr:websites/2021.brightcon.link/
