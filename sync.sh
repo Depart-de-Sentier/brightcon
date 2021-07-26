2e691c7a1a05b48b79c881b60686427a696999c5
Chris Mutel <cmutel@gmail.com>
2021-07-26 14:40:45 +0200

    Update 2021 favicon

--- sync.sh
+++ sync.sh
@@ -5,6 +5,3 @@ rsync -avl --omit-dir-times --no-perms *.ico vultr:websites/2020.brightcon.link/
 rsync -avl --omit-dir-times --no-perms *.webmanifest vultr:websites/2020.brightcon.link/
 
 rsync -avl --omit-dir-times --no-perms 2021/ vultr:websites/2021.brightcon.link/
-rsync -avl --omit-dir-times --no-perms *.png vultr:websites/2021.brightcon.link/
-rsync -avl --omit-dir-times --no-perms *.ico vultr:websites/2021.brightcon.link/
-rsync -avl --omit-dir-times --no-perms *.webmanifest vultr:websites/2021.brightcon.link/
